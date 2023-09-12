fcfe2872aeba8cfc3f362831588a5230eb7af43a
Kirill Doroshuk <kirilldoroshuk@gmail.com>
2023-09-12 21:32:24 +0300

    create sqlite database: dataBase.db implement method to get connection to db via pattern singleton

---
+++ src/main/java/org/study/data/connection/ConnectionDatabaseSingleton.java
@@ -0,0 +1,34 @@
+package org.study.data.connection;
+
+import java.io.File;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConnectionDatabaseSingleton {
+    private static ConnectionDatabaseSingleton singletonConnection = null;
+    private Connection connection = null;
+
+    private ConnectionDatabaseSingleton() {
+        try {
+            String url = "jdbc:sqlite:DataBaseSource" + File.separator + "dataBase.db";
+
+            connection = DriverManager.getConnection(url);
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static synchronized ConnectionDatabaseSingleton getInstance() {
+        if (singletonConnection == null) {
+            singletonConnection = new ConnectionDatabaseSingleton();
+        }
+
+        return singletonConnection;
+    }
+
+    public Connection getConnection() {
+        return connection;
+    }
+}
