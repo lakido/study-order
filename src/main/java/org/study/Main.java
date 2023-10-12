bcd2feedf592f3431bbb7653894d4b6e725d0ad2
Kirill Doroshuk <kirilldoroshuk@gmail.com>
2023-10-12 21:54:37 +0300

    some other changes

--- src/main/java/org/study/Main.java
+++ src/main/java/org/study/Main.java
@@ -1,8 +1,73 @@
 package org.study;
 
+import org.study.data.connection.ConnectionDatabaseSingleton;
+import org.study.data.exceptions.*;
+import org.study.data.operations.extraction.IngredientEntityExtractor;
+import org.study.data.operations.extraction.RecipeEntityExtractor;
+import org.study.data.operations.inserting.RelationRecordInsertWorker;
+
+import java.util.function.Function;
+import java.util.function.Supplier;
+
 public class Main {
-    public static void main(String[] args) {
-        Main main = new Main();
-//        TestConnection.testConnection();
+    interface Test {
+        String concat(Function<String, String> fun);
+    }
+    public static void main(String[] args) throws FailedConnectingException, UnexpectedException, FailedExecuteException, FailedStatementException, FailedReadException {
+//        String x = "boba";
+
+
+//        System.out.println(print(Main::getMess, "error123"));
+
+//        System.out.println(test.concat("biba", "boba"));
+
+//        RelationRecordExtractor relationRecordExtractor = new RelationRecordExtractor(ConnectionDatabaseSingleton.getInstance().getConnection());
+//        System.out.println(relationRecordExtractor.extractListOfRelationRecordsOfRecipeIngredient(9, 15));
+//        RelationRecordInsertWorker relationRecordInsertWorker = new RelationRecordInsertWorker(ConnectionDatabaseSingleton.getInstance().getConnection());
+//        System.out.println(relationRecordInsertWorker.insertRelationRecipeIngredientRecord(9, 15));
+
+//        RelationRecordExtractor relationRecordExtractor = new RelationRecordExtractor(ConnectionDatabaseSingleton.getInstance().getConnection());
+//        RelationRecordInsertWorker relationRecordInsertWorker = new RelationRecordInsertWorker(ConnectionDatabaseSingleton.getInstance().getConnection());
+
+//        RelationDataSource relationDataSource = new RelationDataSource(relationRecordInsertWorker, relationRecordExtractor);
+
+//        System.out.println(relationDataSource.extractListOfRelationIngredientEntitiesByRecipeId(9));
+
+//        System.out.println(relationRecordExtractor.extractRelationRecipeIngredientRecord(9, 9));
+
+//        RecipeEntityExtractor recipeEntityExtractor = new RecipeEntityExtractor(ConnectionDatabaseSingleton.getInstance().getConnection());
+
+//        System.out.println(recipeEntityExtractor.extractRecipeFromDatabase(1));
+
+//        IngredientEntityExtractor entityExtractor = new IngredientEntityExtractor(ConnectionDatabaseSingleton.getInstance().getConnection());
+
+//        System.out.println(entityExtractor.extractIngredientFromDatabaseById(1));
+
+//        RelationRecordInsertWorker relationRecordInsertWorker = new RelationRecordInsertWorker(ConnectionDatabaseSingleton.getInstance().getConnection());
+//        System.out.println(relationRecordInsertWorker.insertRelationRecipeIngredientRecord(45,90));;
+
+
+
+    }
+
+
+    public static String print(Supplier<String> supplier) {
+        try {
+            return supplier.get();
+        } catch (IllegalArgumentException exception) {
+            return "Error";
+        }
+    }
+
+    public static String print(Function<String, String> function, String str) {
+        try {
+            return function.apply(str);
+        } catch (IllegalArgumentException exception) {
+            return exception.getMessage();
+        }
+    }
+
+    public static String getMess(String str1) {
+        throw new IllegalArgumentException("single" + str1);
     }
 }
--- src/main/java/org/study/di/ApplicationComponent.java
+++ src/main/java/org/study/di/ApplicationComponent.java
@@ -1,49 +1,46 @@
 package org.study.di;
 
-import org.study.data.repository.RecipeDataRepository;
-import org.study.data.sources.RecipeDataSource;
-
 public class ApplicationComponent {
 //TODO this class will be creating objects of all other layers and return these
 //TODO i cannot allow the use of foreign objects in layers (dara objects must be used only in data layer)
 
-    private static ApplicationComponent applicationComponent = null;
-
-    private ApplicationComponent() {    }
-
-    public static synchronized ApplicationComponent getInstance() {
-        if (applicationComponent == null) applicationComponent = new ApplicationComponent();
-
-        return applicationComponent;
-    }
-
-    private RecipeDataSource recipeDataSource;
-    private RecipeDataRepository recipeDataRepository;
-
-
-    public synchronized RecipeDataSource provideRecipeDataSource() {
-        return getRecipeDataSource();
-    }
-
-    public synchronized RecipeDataRepository provideDataRepository() {
-        return getRecipeDataRepository();
-    }
-
-
-    private RecipeDataSource getRecipeDataSource() {
-        if (recipeDataSource == null) {
-            recipeDataSource = new RecipeDataSource();
-        }
-
-        return recipeDataSource;
-    }
-
-    private RecipeDataRepository getRecipeDataRepository() {
-        if (recipeDataRepository == null) {
-            recipeDataRepository = new RecipeDataRepository(getRecipeDataSource());
-        }
-
-        return recipeDataRepository;
-    }
+//    private static ApplicationComponent applicationComponent = null;
+//
+//    private ApplicationComponent() {    }
+//
+//    public static synchronized ApplicationComponent getInstance() {
+//        if (applicationComponent == null) applicationComponent = new ApplicationComponent();
+//
+//        return applicationComponent;
+//    }
+//
+//    private RecipeDataSource recipeDataSource;
+//    private RecipeDataRepository recipeDataRepository;
+//
+//
+//    public synchronized RecipeDataSource provideRecipeDataSource() {
+//        return getRecipeDataSource();
+//    }
+//
+//    public synchronized RecipeDataRepository provideDataRepository() {
+//        return getRecipeDataRepository();
+//    }
+//
+//
+//    private RecipeDataSource getRecipeDataSource() {
+//        if (recipeDataSource == null) {
+//            recipeDataSource = new RecipeDataSource();
+//        }
+//
+//        return recipeDataSource;
+//    }
+//
+//    private RecipeDataRepository getRecipeDataRepository() {
+//        if (recipeDataRepository == null) {
+//            recipeDataRepository = new RecipeDataRepository(getRecipeDataSource());
+//        }
+//
+//        return recipeDataRepository;
+//    }
 
 }
