959769a9b7cd19851b05315ca1be556bf66b6995
Kirill Doroshuk <kirilldoroshuk@gmail.com>
2023-11-14 23:49:40 +0300

    create screens for ingredient adding

---
+++ src/main/java/org/study/ui/controllers/IngredientAddingController.java
@@ -0,0 +1,86 @@
+package org.study.ui.controllers;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.*;
+import javafx.scene.layout.GridPane;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import java.util.function.UnaryOperator;
+import java.util.regex.Pattern;
+
+public class IngredientAddingController implements Initializable {
+    @FXML
+    public GridPane gridPaneIngredientAdding;
+    @FXML
+    public TextField ingredientNameTextField;
+    @FXML
+    public ComboBox<String> ingredientRecommendationComboBox;
+    @FXML
+    public Spinner<Integer> ingredientWeightSpinner;
+    @FXML
+    public Spinner<Integer> ingredientCaloriesSpinner;
+    @FXML
+    public Button addIngredientButton;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        restrictionForIngredientWeightSpinner();
+        restrictionForIngredientCaloriesSpinner();
+        restrictionForTextField();
+    }
+
+    private void restrictionForIngredientWeightSpinner() {
+        SpinnerValueFactory<Integer> recipePopularitySpinnerValueFactory = new SpinnerValueFactory.IntegerSpinnerValueFactory(100, 10000, 300, 100);
+
+        UnaryOperator<TextFormatter.Change> filter = change -> {
+            String newText = change.getControlNewText();
+
+            if (Pattern.matches("\\d{0,5}", newText)) {
+                return change;
+            } else {
+                return null;
+            }
+        };
+
+        TextFormatter<Integer> textFormatter = new TextFormatter<>(recipePopularitySpinnerValueFactory.getConverter(), recipePopularitySpinnerValueFactory.getValue(), filter);
+
+        ingredientWeightSpinner.setValueFactory(recipePopularitySpinnerValueFactory);
+        ingredientWeightSpinner.getEditor().setTextFormatter(textFormatter);
+    }
+
+    private void restrictionForIngredientCaloriesSpinner() {
+        SpinnerValueFactory<Integer> recipeAgePreferencesSpinnerValueFactory = new SpinnerValueFactory.IntegerSpinnerValueFactory(0, 20000, 200, 50);
+
+        UnaryOperator<TextFormatter.Change> filter = change -> {
+            String newText = change.getControlNewText();
+
+            if (Pattern.matches("\\d{0,5}", newText)) {
+                return change;
+            } else {
+                return null;
+            }
+        };
+
+        TextFormatter<Integer> textFormatter = new TextFormatter<>(recipeAgePreferencesSpinnerValueFactory.getConverter(), recipeAgePreferencesSpinnerValueFactory.getValue(), filter);
+
+        ingredientCaloriesSpinner.setValueFactory(recipeAgePreferencesSpinnerValueFactory);
+        ingredientCaloriesSpinner.getEditor().setTextFormatter(textFormatter);
+    }
+
+    private void restrictionForTextField() {
+        UnaryOperator<TextFormatter.Change> filter = change -> {
+            String newText = change.getControlNewText();
+
+            if (newText.matches("[^0-9!@#$%^&*()]{0,100}")) {
+                return change;
+            } else {
+                return null;
+            }
+        };
+
+        TextFormatter<String> textFormatter = new TextFormatter<>(filter);
+        ingredientNameTextField.setTextFormatter(textFormatter);
+    }
+}
---
+++ src/main/java/org/study/ui/screens/IngredientAddingScreen.java
@@ -0,0 +1,11 @@
+package org.study.ui.screens;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class IngredientAddingScreen extends Application {
+    @Override
+    public void start(Stage stage) throws Exception {
+        stage.show();
+    }
+}
