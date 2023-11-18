4296ffb6e6d1db6cc91e355a5c114053b23f000e
Kirill Doroshuk <kirilldoroshuk@gmail.com>
2023-11-18 20:07:46 +0300

    implement recipe adding via ui

--- src/main/java/org/study/ui/controllers/RecipeAddingController.java
+++ src/main/java/org/study/ui/controllers/RecipeAddingController.java
@@ -12,11 +12,38 @@ import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.*;
 import javafx.stage.Stage;
+import org.study.data.connection.ConnectionDatabaseSingleton;
+import org.study.data.exceptions.FailedConnectingException;
 import org.study.data.exceptions.UnexpectedException;
+import org.study.data.operations.changing.IngredientUpdateWorker;
+import org.study.data.operations.changing.RecipeUpdateWorker;
+import org.study.data.operations.deletion.IngredientDeleteWorker;
+import org.study.data.operations.deletion.RecipeDeleteWorker;
+import org.study.data.operations.extraction.IngredientEntityExtractor;
+import org.study.data.operations.extraction.RecipeEntityExtractor;
+import org.study.data.operations.extraction.RelationRecordExtractor;
+import org.study.data.operations.inserting.IngredientInsertWorker;
+import org.study.data.operations.inserting.RecipeInsertWorker;
+import org.study.data.operations.inserting.RelationRecordInsertWorker;
+import org.study.data.repository.IngredientDataRepository;
+import org.study.data.repository.RecipeDataRepository;
+import org.study.data.repository.RelationDataRepository;
+import org.study.data.sources.ingredient.IngredientDataSource;
+import org.study.data.sources.recipe.RecipeDataSource;
+import org.study.data.sources.relation.RelationDataSource;
 import org.study.domain.models.IngredientModel;
+import org.study.domain.models.RecipeModel;
+import org.study.domain.repository.RelationRepository;
+import org.study.domain.usecases.ingredient.ExtractNextAvailableIdForIngredientUseCase;
+import org.study.domain.usecases.ingredient.InsertIngredientUseCase;
+import org.study.domain.usecases.recipe.ExtractNextAvailableIdForRecipeUseCase;
+import org.study.domain.usecases.recipe.InsertRecipeUseCase;
+import org.study.domain.usecases.relation.InsertRelationUseCase;
 
 import java.io.IOException;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.ResourceBundle;
 import java.util.function.UnaryOperator;
 import java.util.regex.Pattern;
@@ -49,6 +76,12 @@ public class RecipeAddingController implements Initializable {
     public TableColumn<IngredientModel, String> ingredientRecommendationColumn;
 
     private final ObservableList<IngredientModel> observableList = FXCollections.observableArrayList();
+
+    private RecipeDataRepository recipeDataRepository;
+
+    private IngredientDataRepository ingredientDataRepository;
+
+    private RelationRepository relationRepository;
 
     public RecipeAddingController() {}
 
@@ -153,5 +186,76 @@ public class RecipeAddingController implements Initializable {
 
         TextFormatter<String> textFormatter = new TextFormatter<>(filter);
         recipeNameTextField.setTextFormatter(textFormatter);
+    }
+
+    @FXML
+    public void handleButtonToInsertNewRecipeIntoDatabase(ActionEvent actionEvent) throws FailedConnectingException {
+        initRecipeDataRepository();
+        initIngredientDataRepository();
+        initRelationRepository();
+        addRecipe();
+
+        Stage stage = (Stage) addIngredientButton.getScene().getWindow();
+        stage.close();
+    }
+
+    private void initRecipeDataRepository() throws FailedConnectingException {
+        RecipeUpdateWorker recipeUpdateWorker = RecipeUpdateWorker.getInstance(ConnectionDatabaseSingleton.getInstance().getConnection());
+        RecipeEntityExtractor recipeEntityExtractor = RecipeEntityExtractor.getInstance(ConnectionDatabaseSingleton.getInstance().getConnection());
+        RecipeInsertWorker recipeInsertWorker = RecipeInsertWorker.getInstance(ConnectionDatabaseSingleton.getInstance().getConnection());
+        RecipeDeleteWorker recipeDeleteWorker = RecipeDeleteWorker.getInstance(ConnectionDatabaseSingleton.getInstance().getConnection());
+
+        RecipeDataSource recipeDataSource = RecipeDataSource.getInstance(recipeUpdateWorker, recipeDeleteWorker, recipeEntityExtractor, recipeInsertWorker);
+        this.recipeDataRepository = RecipeDataRepository.getInstance(recipeDataSource);
+    }
+
+    private void initIngredientDataRepository() throws FailedConnectingException {
+        IngredientUpdateWorker ingredientUpdateWorker = IngredientUpdateWorker.getInstance(ConnectionDatabaseSingleton.getInstance().getConnection());
+        IngredientEntityExtractor ingredientEntityExtractor = IngredientEntityExtractor.getInstance(ConnectionDatabaseSingleton.getInstance().getConnection());
+        IngredientInsertWorker ingredientInsertWorker = IngredientInsertWorker.getInstance(ConnectionDatabaseSingleton.getInstance().getConnection());
+        IngredientDeleteWorker ingredientDeleteWorker = IngredientDeleteWorker.getInstance(ConnectionDatabaseSingleton.getInstance().getConnection());
+
+        IngredientDataSource ingredientDataSource = IngredientDataSource.getInstance(ingredientUpdateWorker, ingredientDeleteWorker, ingredientEntityExtractor, ingredientInsertWorker);
+        this.ingredientDataRepository = IngredientDataRepository.getInstance(ingredientDataSource);
+    }
+
+    private void initRelationRepository() throws FailedConnectingException {
+        RelationRecordInsertWorker relationRecordInsertWorker = RelationRecordInsertWorker.getInstance(ConnectionDatabaseSingleton.getInstance().getConnection());
+        RelationRecordExtractor relationRecordExtractor = RelationRecordExtractor.getInstance(ConnectionDatabaseSingleton.getInstance().getConnection());
+
+        RelationDataSource relationDataSource = RelationDataSource.getInstance(relationRecordInsertWorker, relationRecordExtractor);
+        this.relationRepository = RelationDataRepository.getInstance(relationDataSource);
+    }
+
+    private void addRecipe() {
+        ExtractNextAvailableIdForRecipeUseCase availableIdForRecipeUseCase = new ExtractNextAvailableIdForRecipeUseCase(recipeDataRepository);
+        ExtractNextAvailableIdForIngredientUseCase availableIdForIngredientUseCase = new ExtractNextAvailableIdForIngredientUseCase(ingredientDataRepository);
+
+        InsertRecipeUseCase insertRecipeUseCase = new InsertRecipeUseCase(recipeDataRepository);
+
+        RecipeModel recipeModel = new RecipeModel(
+                1,
+                recipeNameTextField.getText(),
+                recipeCategoryComboBox.getValue(),
+                recipePopularitySpinner.getValue(),
+                recipeAgePreferencesSpinner.getValue()
+        );
+
+        List<IngredientModel> ingredientModelList = new ArrayList<>(observableList);
+
+        int recipeId = availableIdForRecipeUseCase.invoke().getOrNull();
+        int ingredientId = availableIdForIngredientUseCase.invoke().getOrNull();
+
+        insertRecipeUseCase.invoke(recipeModel);
+
+        for (IngredientModel ingredient: ingredientModelList
+        ) {
+            InsertIngredientUseCase insertIngredientUseCase = new InsertIngredientUseCase(ingredientDataRepository);
+            InsertRelationUseCase insertRelationUseCase = new InsertRelationUseCase(relationRepository);
+
+            insertIngredientUseCase.invoke(ingredient);
+            insertRelationUseCase.invoke(recipeId, ingredientId);
+            ingredientId++;
+        }
     }
 }
