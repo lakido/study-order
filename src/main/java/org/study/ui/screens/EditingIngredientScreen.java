f8ba61bcc623cca38b0e2ea58e8f862f2b29da03
Kirill Doroshuk <kirilldoroshuk@gmail.com>
2023-11-28 21:31:30 +0300

    create window for ingredient editing in context menu for recipe

---
+++ src/main/java/org/study/ui/screens/EditingIngredientScreen.java
@@ -0,0 +1,11 @@
+package org.study.ui.screens;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class EditingIngredientScreen extends Application {
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        primaryStage.show();
+    }
+}
