c539b627ab2cc12c857a1426a8eacababb6ae848
Kirill Doroshuk <kirilldoroshuk@gmail.com>
2024-01-14 19:44:43 +0300

    create and use method to init db file

--- src/main/java/org/study/data/connection/ConnectionDatabaseSingleton.java
+++ src/main/java/org/study/data/connection/ConnectionDatabaseSingleton.java
@@ -1,15 +1,20 @@
 package org.study.data.connection;
 
 import org.study.data.exceptions.FailedConnectingException;
+import org.study.data.init.CheckDatabase;
+import org.study.data.init.CheckFolderForDataBase;
 
 import java.io.File;
+import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 public class ConnectionDatabaseSingleton {
 
     private static ConnectionDatabaseSingleton singletonConnection = null;
     private final String dataBaseName = "dataBase.db";
+    private final String dataBaseLocation = "jdbc:sqlite:DataBaseSource";
 
     private ConnectionWrapper connection = null;
 
@@ -30,17 +35,17 @@ public class ConnectionDatabaseSingleton {
         return connection;
     }
 
-    public String getDataBaseName() {
-        return dataBaseName;
-    }
-
     private synchronized void createConnection() throws FailedConnectingException {
-        String dataBaseLocation = "jdbc:sqlite:DataBaseSource";
-
+        init();
         try {
             connection = new ConnectionWrapper(DriverManager.getConnection(dataBaseLocation + File.separator + dataBaseName));
         } catch (SQLException e) {
             throw new FailedConnectingException();
         }
+    }
+
+    private void init() throws FailedConnectingException{
+        CheckFolderForDataBase.checkFolder();
+        CheckDatabase.checkDatabaseStatus();
     }
 }
